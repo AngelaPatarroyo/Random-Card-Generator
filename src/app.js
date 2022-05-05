4895a00c9bdc0ec8b457a129142ac3963a7c5f66
AngelaPatarroyo <angela.patarroyo@hotmail.com>
2022-05-05 02:46:04 +0000

    cambios

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let card = generarcarta();
+
+  document.querySelector(".topicon").innerHTML = card[0];
+  document.querySelector(".number").innerHTML = card[1];
+  document.querySelector(".bottomicon").innerHTML = card[0];
+};
+let generarcarta = () => {
+  let iconos = ["♦", "♥", "♠", "♣"];
+  let simbolo = ["3", "J", "8", "A", "2", "4", "5", "6", "7", "9", "Q", "K"];
+
+  let num1 = Math.floor(Math.random() * iconos.length);
+  let num2 = Math.floor(Math.random() * simbolo.length);
+  console.log(num1);
+  return [iconos[num1], simbolo[num2]];
 };
